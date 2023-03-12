3683931468a096959651cbc33577e8caead405f5
Andrew <iAndrew3@protonmail.com>
2023-03-12 17:19:07 +0200

    about section

--- components/sections/about.js
+++ components/sections/about.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import Image from 'next/image'
 import { gsap } from 'gsap/dist/gsap';
 
 import Menu from '../ui/menu';
@@ -10,11 +11,20 @@ function About({}) {
 				trigger: '.article',
 				scrub: 0.5,
 				start: 'top top',
+				end: '+=500%',
 				pin: '.about-section',
 			}
-		}).to('.article', {
-			y: "-=50%"
-		});
+		}).to('.html-tech-stack', {
+			opacity: 0
+		}, '>').to('.javascript-tech-stack', {
+			opacity: 0
+		}, '>').to('.nodejs-tech-stack', {
+			opacity: 0
+		}, '>').to('.mongodb-tech-stack', {
+			opacity: 0
+		}, '>').to('.git-tech-stack', {
+			opacity: 0
+		}, '>');
 	}, []);
 
 	return (
@@ -36,7 +46,57 @@ function About({}) {
 				</div>
 			</div>
 			<div className="h-[200vh] w-[50vw] article bg-white p-[96px]" id="about-right">
-				At NativCodes, we build web and mobile apps. That&apos;s it.
+				<h1 className="text-4xl font-bold text-dark mb-5">Hello world!</h1>
+				<div className="text-lg text-dark mt-2">Welcome to <span className="italic">NativCodes</span>, the place where we play with <span className="italic">The Code</span> and build awesome apps.</div>
+				<div className="text-lg text-dark mt-2">We are a team of tech enthusiasts who thrive on solving complex problems and transforming ideas into reality. Our expertise lies in developing native applications for iOS and Android platforms, as well as building responsive websites using cutting-edge technologies.</div>
+				<div className="text-lg text-dark mt-2">At <span className="italic">NativCodes</span>, we are committed to staying on the forefront of emerging technologies and trends. We continuously research and experiment with new tools and techniques, allowing us to provide our apps with the most performant, scalable, and secure implementations.</div>
+
+				<div className="flex flex-row items-center flex-wrap justify-center mt-2">
+					<div className="relative w-[200px] h-[200px]">
+						<div className="absolute css-tech-stack">
+							<Image src="/assets/images/tech/css-logo.png" alt="css logo" width="200" height="200" />
+						</div>
+						<div className="absolute html-tech-stack">
+							<Image src="/assets/images/tech/html-logo.png" alt="html logo" width="200" height="200" />
+						</div>
+					</div>
+
+					<div className="relative w-[200px] h-[200px]">
+						<div className="absolute react-tech-stack">
+							<Image src="/assets/images/tech/react-logo.png" alt="react logo" width="200" height="200" />
+						</div>
+						<div className="absolute javascript-tech-stack">
+							<Image src="/assets/images/tech/javascript-logo.png" alt="javascript logo" width="200" height="200" />
+						</div>
+					</div>
+
+					<div className="relative w-[200px] h-[200px]">
+						<div className="absolute php-tech-stack">
+							<Image src="/assets/images/tech/php-logo.png" alt="php logo" width="200" height="200" />
+						</div>
+						<div className="absolute nodejs-tech-stack">
+							<Image src="/assets/images/tech/nodejs-logo.png" alt="nodejs logo" width="200" height="200" />
+						</div>
+					</div>
+
+					<div className="relative w-[200px] h-[200px]">
+						<div className="absolute mysql-tech-stack">
+							<Image src="/assets/images/tech/mysql-logo.png" alt="mysql logo" width="200" height="200" />
+						</div>
+						<div className="absolute mongodb-tech-stack">
+							<Image src="/assets/images/tech/mongodb-logo.png" alt="mongodb logo" width="200" height="200" />
+						</div>
+					</div>
+
+					<div className="relative w-[200px] h-[200px]">
+						<div className="absolute docker-tech-stack">
+							<Image src="/assets/images/tech/docker-logo.png" alt="docker logo" width="200" height="200" />
+						</div>
+						<div className="absolute git-tech-stack">
+							<Image src="/assets/images/tech/git-logo.png" alt="git logo" width="200" height="200" />
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
--- components/ui/menu.js
+++ components/ui/menu.js
@@ -6,7 +6,7 @@ function Menu({theme}) {
 			home: 0,
 			about: window.innerHeight,
 			work: window.innerHeight * 4,
-			contact: window.innerHeight * 18
+			contact: window.innerHeight * 21
 		}
 
 		window.scrollTo({
