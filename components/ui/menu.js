6620583b7439bad11c73b3dcdef3c8eaf7b57f41
Andrew <iAndrew3@protonmail.com>
2024-02-04 17:02:06 +0200

    fixed timelines and menu

--- components/ui/menu.js
+++ components/ui/menu.js
@@ -1,38 +1,48 @@
-import {useResponsive} from '../../utils/use-responsive-timeline';
-const sections = ['home', '/', 'about', '/', 'work', '/', 'contact'];
+import { useResponsive } from "../../utils/use-responsive-timeline";
+const sections = ["home", "/", "about", "/", "work", "/", "contact"];
 
-function Menu({theme}) {
-	const isSmallScreen = useResponsive();
+function Menu({ theme }) {
+  const isSmallScreen = useResponsive();
 
-	const scrollTo = page => () => {
-		const largeScreenMapper = {
-			home: 0,
-			about: window.innerHeight * 2,
-			work: window.innerHeight * 4,
-			contact: window.innerHeight * 18
-		}
-		const smallScreenMapper = {
-			home: 0,
-			about: window.innerHeight * 2,
-			work: window.innerHeight * 5.5,
-			contact: window.innerHeight * 21
-		}
+  const scrollTo = (page) => () => {
+    const largeScreenMapper = {
+      home: 0,
+      about: window.innerHeight * 2,
+      work: window.innerHeight * 4,
+      contact: window.innerHeight * 7,
+    };
+    const smallScreenMapper = {
+      home: 0,
+      about: window.innerHeight * 2,
+      work: window.innerHeight * 6,
+      contact: window.innerHeight * 11,
+    };
 
-		window.scrollTo({
-			top: (isSmallScreen ? smallScreenMapper : largeScreenMapper)[page],
-			behavior: 'smooth'
-		});
-	}
+    window.scrollTo({
+      top: (isSmallScreen ? smallScreenMapper : largeScreenMapper)[page],
+      behavior: "smooth",
+    });
+  };
 
-	return (
-		<ul className={`flex flex-row text-${theme} my-8`}>
-			{sections.map((page, index) => page === '/' ? (
-				<li key={`${page}-${index}`} className="px-2 font-bold text-sm">{page}</li>
-			) : (
-				<li key={`${page}-${index}`} className="px-2 font-bold text-sm cursor-pointer" onClick={scrollTo(page)}>{page.toUpperCase()}</li>
-			))}
-		</ul>
-	)
+  return (
+    <ul className={`flex flex-row text-${theme} my-8`}>
+      {sections.map((page, index) =>
+        page === "/" ? (
+          <li key={`${page}-${index}`} className="px-2 font-bold text-sm">
+            {page}
+          </li>
+        ) : (
+          <li
+            key={`${page}-${index}`}
+            className="px-2 font-bold text-sm cursor-pointer"
+            onClick={scrollTo(page)}
+          >
+            {page.toUpperCase()}
+          </li>
+        )
+      )}
+    </ul>
+  );
 }
 
 export default Menu;
--- utils/timelines.js
+++ utils/timelines.js
@@ -1,224 +1,324 @@
-import { gsap } from 'gsap/dist/gsap';
+import { gsap } from "gsap/dist/gsap";
 
 function getAboutLargeScreenTimeline() {
-	return gsap.timeline({
-		scrollTrigger: {
-			// markers: true,
-			trigger: '.about-section',
-			scrub: 0.5,
-			start: 'top top',
-			end: '+=100%',
-			pin: true,
-			pinSpacing: true
-		}
-	}).to('.html-tech-stack', {
-		opacity: 0
-	}, '>').to('.javascript-tech-stack', {
-		opacity: 0
-	}, '>').to('.nodejs-tech-stack', {
-		opacity: 0
-	}, '>').to('.mongodb-tech-stack', {
-		opacity: 0
-	}, '>');
+  return gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".about-section",
+        scrub: 0.5,
+        start: ".about-section",
+        pin: true,
+        pinSpacing: true,
+      },
+    })
+    .to(
+      ".html-tech-stack",
+      {
+        opacity: 0,
+      },
+      ">"
+    )
+    .to(
+      ".javascript-tech-stack",
+      {
+        opacity: 0,
+      },
+      ">"
+    )
+    .to(
+      ".nodejs-tech-stack",
+      {
+        opacity: 0,
+      },
+      ">"
+    )
+    .to(
+      ".mongodb-tech-stack",
+      {
+        opacity: 0,
+      },
+      ">"
+    );
 }
 
 function getAboutLeftSmallScreenTimeline() {
-	return gsap.timeline({
-		scrollTrigger: {
-			// markers: true,
-			trigger: '.about-section-left',
-			scrub: 0.5,
-			start: 'top top',
-			end: '+=50%',
-			pin: true,
-			pinSpacing: true
-		}
-	});
+  return gsap.timeline({
+    scrollTrigger: {
+      trigger: ".about-section-left",
+      scrub: 0.5,
+      start: ".about-section-left",
+      pin: true,
+      pinSpacing: true,
+    },
+  });
 }
 
 function getAboutRightSmallScreenTimeline() {
-	return gsap.timeline({
-		scrollTrigger: {
-			// markers: true,
-			trigger: '.about-section-right',
-			scrub: 0.5,
-			start: 'top top',
-			end: '+=100%',
-			pin: true,
-			pinSpacing: true
-		}
-	}).to('.html-tech-stack', {
-		opacity: 0
-	}, '>').to('.javascript-tech-stack', {
-		opacity: 0
-	}, '>').to('.nodejs-tech-stack', {
-		opacity: 0
-	}, '>').to('.mongodb-tech-stack', {
-		opacity: 0
-	}, '>');
+  return gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".about-section-right",
+        scrub: 0.5,
+        start: ".about-section-right",
+        pin: true,
+        pinSpacing: true,
+      },
+    })
+    .to(
+      ".html-tech-stack",
+      {
+        opacity: 0,
+      },
+      ">"
+    )
+    .to(
+      ".javascript-tech-stack",
+      {
+        opacity: 0,
+      },
+      ">"
+    )
+    .to(
+      ".nodejs-tech-stack",
+      {
+        opacity: 0,
+      },
+      ">"
+    )
+    .to(
+      ".mongodb-tech-stack",
+      {
+        opacity: 0,
+      },
+      ">"
+    );
 }
 
 function getWorkLargeScreenTimeline() {
-	const listraFramesCount = 38;
-	const listraFrameWidth = 316;
+  const listraFramesCount = 38;
+  const listraFrameWidth = 316;
 
-	return gsap.timeline({
-		scrollTrigger: {
-			// markers: true,
-			trigger: ".work-section",
-			start: "top top",
-			end: "+=" + (listraFramesCount * listraFrameWidth),
-			pin: true,
-			scrub: true
-		}
-	}).to("#work-listra", {
-	  backgroundPosition: (-listraFrameWidth * listraFramesCount) + "px 0px",
-	  ease: "steps(" + listraFramesCount + ")", // use a stepped ease for the sprite sheet
-	}, 0).to("#download-listra", {
-	  opacity: 1
-	}, ">").to("#sprite-listra", {
-	  opacity: 0
-	}, "<");
+  return gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".work-section",
+        start: ".work-section",
+        end: "+=200%",
+        pin: true,
+        scrub: true,
+      },
+    })
+    .to(
+      "#work-listra",
+      {
+        backgroundPosition: -listraFrameWidth * listraFramesCount + "px 0px",
+        ease: "steps(" + listraFramesCount + ")", // use a stepped ease for the sprite sheet
+      },
+      0
+    )
+    .to(
+      "#download-listra",
+      {
+        opacity: 1,
+      },
+      ">"
+    )
+    .to(
+      "#sprite-listra",
+      {
+        opacity: 0,
+      },
+      "<"
+    );
 }
 
 function getWorkLeftSmallScreenTimeline() {
-	return gsap.timeline({
-		scrollTrigger: {
-			// markers: true,
-			trigger: '.work-section-left',
-			scrub: 0.5,
-			start: 'top top',
-			end: '+=50%',
-			pin: true,
-			pinSpacing: true
-		}
-	});
+  return gsap.timeline({
+    scrollTrigger: {
+      trigger: ".work-section-left",
+      scrub: 0.5,
+      start: ".work-section-left",
+      pin: true,
+      pinSpacing: true,
+    },
+  });
 }
 
 function getWorkRightSmallScreenTimeline() {
-	const listraFramesCount = 38;
-	const listraFrameWidth = 316;
+  const listraFramesCount = 38;
+  const listraFrameWidth = 316;
 
-	return gsap.timeline({
-		scrollTrigger: {
-			// markers: true,
-			trigger: ".work-section-right",
-			start: "top top",
-			end: "+=" + (listraFramesCount * listraFrameWidth),
-			pin: true,
-			scrub: true,
-			pinSpacing: true
-		}
-	}).to("#work-listra", {
-	  backgroundPosition: (-listraFrameWidth * listraFramesCount) + "px 0px",
-	  ease: "steps(" + listraFramesCount + ")", // use a stepped ease for the sprite sheet
-	}, 0).to("#download-listra", {
-	  opacity: 1
-	}, ">").to("#sprite-listra", {
-	  opacity: 0
-	}, "<");
+  return gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".work-section-right",
+        start: ".work-section-right",
+        end: "+=200%",
+        pin: true,
+        scrub: true,
+        pinSpacing: true,
+      },
+    })
+    .to(
+      "#work-listra",
+      {
+        backgroundPosition: -listraFrameWidth * listraFramesCount + "px 0px",
+        ease: "steps(" + listraFramesCount + ")", // use a stepped ease for the sprite sheet
+      },
+      0
+    )
+    .to(
+      "#download-listra",
+      {
+        opacity: 1,
+      },
+      ">"
+    )
+    .to(
+      "#sprite-listra",
+      {
+        opacity: 0,
+      },
+      "<"
+    );
 }
 
 function getContactLargeScreenTimeline() {
-	return gsap.timeline({
-		scrollTrigger: {
-			trigger: '.contact-section',
-			scrub: 1,
-			start: 'top top',
-			pin: '.contact-section',
-			end: '+=50%',
-		}
-	});
+  return gsap.timeline({
+    scrollTrigger: {
+      trigger: ".contact-section",
+      scrub: 1,
+      start: ".contact-section",
+      pin: true,
+    },
+  });
 }
 
 function getContactLeftSmallScreenTimeline() {
-	return gsap.timeline({
-		scrollTrigger: {
-			// markers: true,
-			trigger: '.contact-section-left',
-			scrub: 0.5,
-			start: 'top top',
-			end: '+=50%',
-			pin: true,
-			pinSpacing: true
-		}
-	});
+  return gsap.timeline({
+    scrollTrigger: {
+      trigger: ".contact-section-left",
+      scrub: 0.5,
+      start: ".contact-section-left",
+      pin: true,
+      pinSpacing: true,
+    },
+  });
 }
 
 function getContactRightSmallScreenTimeline() {
-	return gsap.timeline({
-		scrollTrigger: {
-			// markers: true,
-			trigger: '.contact-section-right',
-			scrub: 0.5,
-			start: 'top top',
-			end: '+=50%',
-			pin: true,
-			pinSpacing: true
-		}
-	});
+  return gsap.timeline({
+    scrollTrigger: {
+      trigger: ".contact-section-right",
+      scrub: 0.5,
+      start: ".contact-section-left",
+      pin: true,
+      pinSpacing: true,
+    },
+  });
 }
 
 function getFooterTimeline() {
-	return gsap.timeline({
-		scrollTrigger: {
-			trigger: '.footer-section',
-			scrub: 0.5,
-			end: '+=200%',
-			pin: true,
-			pinSpacing: true
-		}
-	}).to('.logo-expanded-text', {
-		opacity: 0,
-		duration: 0.2
-	}, 0).to('.logo-expanded-left-arrow', {
-		translateX: -280,
-		translateY: -50,
-	}, 0).to('.logo-expanded-right-arrow', {
-		translateX: 280,
-		translateY: 90,
-	}, 0).to('.logo-expanded', {
-		rotation: -90
-	}, 0);
+  return gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".footer-section",
+        scrub: 0.5,
+        start: ".footer-section",
+        pin: true,
+        pinSpacing: true,
+      },
+    })
+    .to(
+      ".logo-expanded-text",
+      {
+        opacity: 0,
+        duration: 0.2,
+      },
+      0
+    )
+    .to(
+      ".logo-expanded-left-arrow",
+      {
+        translateX: -280,
+        translateY: -50,
+      },
+      0
+    )
+    .to(
+      ".logo-expanded-right-arrow",
+      {
+        translateX: 280,
+        translateY: 90,
+      },
+      0
+    )
+    .to(
+      ".logo-expanded",
+      {
+        rotation: -90,
+      },
+      0
+    );
 }
 
 function getHeaderTimeline() {
-	return gsap.timeline({
-		scrollTrigger: {
-			trigger: '.header-section',
-			end: '+=100%',
-			pin: true,
-			scrub: 0.5,
-			// pinSpacing: false
-		}
-	}).to('.logo-small-left-arrow', {
-		translateX: 0,
-		translateY: 0,
-	}, 0).to('.logo-small-right-arrow', {
-		translateX: 0,
-		translateY: 0,
-	}, 0).to('.logo-small-text', {
-		opacity: 1
-	}, '>').to('.logo-small', {
-		rotation: 0
-	}, 0);
+  return gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".header-section",
+        start: ".header-section",
+        pin: true,
+        scrub: 0.5,
+      },
+    })
+    .to(
+      ".logo-small-left-arrow",
+      {
+        translateX: 0,
+        translateY: 0,
+      },
+      0
+    )
+    .to(
+      ".logo-small-right-arrow",
+      {
+        translateX: 0,
+        translateY: 0,
+      },
+      0
+    )
+    .to(
+      ".logo-small-text",
+      {
+        opacity: 1,
+      },
+      ">"
+    )
+    .to(
+      ".logo-small",
+      {
+        rotation: 0,
+      },
+      0
+    );
 }
 
 export {
-	// About
-	getAboutLargeScreenTimeline,
-	getAboutLeftSmallScreenTimeline,
-	getAboutRightSmallScreenTimeline,
-	// Work
-	getWorkLargeScreenTimeline,
-	getWorkLeftSmallScreenTimeline,
-	getWorkRightSmallScreenTimeline,
-	// Contact
-	getContactLargeScreenTimeline,
-	getContactLeftSmallScreenTimeline,
-	getContactRightSmallScreenTimeline,
-	// Footer
-	getFooterTimeline,
-	// Header
-	getHeaderTimeline
+  // About
+  getAboutLargeScreenTimeline,
+  getAboutLeftSmallScreenTimeline,
+  getAboutRightSmallScreenTimeline,
+  // Work
+  getWorkLargeScreenTimeline,
+  getWorkLeftSmallScreenTimeline,
+  getWorkRightSmallScreenTimeline,
+  // Contact
+  getContactLargeScreenTimeline,
+  getContactLeftSmallScreenTimeline,
+  getContactRightSmallScreenTimeline,
+  // Footer
+  getFooterTimeline,
+  // Header
+  getHeaderTimeline,
 };
