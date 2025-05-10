14c8dd80822f8b1b0ca2156f67301c8abe014377
Andrei Otea <iAndrew3@protonmail.com>
2025-05-11 01:49:41 +0300

    updated page

--- app/legal/xido/page.js
+++ app/legal/xido/page.js
@@ -2,15 +2,16 @@ import { PrivacyPolicy } from '../../../constants/legal';
 
 function Xido() {
   return (
-    <section className="flex lg:h-screen w-full flex-col sm:flex-row">
-      <div className="bg-dark relative flex justify-center items-center h-screen sm:w-1/4">
+    <section className="flex w-full flex-col sm:flex-row">
+      <div className="bg-dark sm:fixed sm:top-0 sm:left-0 flex justify-center items-center h-screen w-full sm:w-1/4">
         <div className="font-[LemonMilk] text-white text-5xl tracking-wider">
           XIDO
         </div>
-        <div className="hexagon-icon hidden z-10 lg:block absolute right-[-65px] top-[96px]">
-          <svg viewBox="0 0 512 512" width="130" height="130">
-            <clipPath id="hexagon">
-              <path
+      </div>
+      <div className="hidden sm:block absolute left-[calc(25%-65px)] top-[96px] z-[9999] pointer-events-none">
+        <svg viewBox="0 0 512 512" width="130" height="130">
+          <clipPath id="hexagon">
+            <path
                 id="heart"
                 d="M485.291,129.408l-224-128c-3.285-1.877-7.296-1.877-10.581,0l-224,128c-3.328,1.899-5.376,5.44-5.376,9.259v234.667
 							c0,3.819,2.048,7.36,5.376,9.259l224,128c1.643,0.939,3.456,1.408,5.291,1.408s3.648-0.469,5.291-1.408l224-128
@@ -26,8 +27,7 @@ function Xido() {
             />
           </svg>
         </div>
-      </div>
-      <div className="about-section-right relative bg-white p-[56px] lg:p-[96px] h-screen sm:w-3/4 overflow-y-auto">
+      <div className="about-section-right relative bg-white p-[56px] sm:p-[96px] min-h-screen w-full sm:ml-[25%] sm:w-3/4 overflow-y-auto">
         <h1 className="text-3xl sm:text-4xl font-bold text-dark mb-5">
           {PrivacyPolicy.title}
         </h1>
